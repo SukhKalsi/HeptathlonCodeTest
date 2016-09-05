c0d314e39536273dd8e2029416b23e113a61c1df
Sukh Kalsi <SukhKalsi@users.noreply.github.com>
2016-09-05 23:31:48 +0100

    Remove echo.

--- autoloader.php
+++ autoloader.php
@@ -2,7 +2,7 @@
 
 function __autoload($className) {
     $file = ROOT_PATH . '/classes/' . $className . '.php';
-    echo $file . PHP_EOL;
+
     if (file_exists($file)) { 
         require $file;
         return true;
